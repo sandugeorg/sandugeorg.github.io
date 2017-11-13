ac2e97809e0d9539cec8156e42a3574a67438a01
sandugeorg <sandu.georg@gmail.com>
2017-11-13 19:26:19 +0200

    am adaugat sir

--- js/senzori.js
+++ js/senzori.js
@@ -4,9 +4,9 @@ function on_device_orientation(e)
 
 {
 	
-document.getElementById("id_z").innerHTML = e.alpha;
-document.getElementById("id_x").innerHTML = e.beta;
-document.getElementById("id_y").innerHTML = e.gamma;
+document.getElementById("id_z").innerHTML = "z=" + e.alpha;
+document.getElementById("id_x").innerHTML = "x=" +e.beta;
+document.getElementById("id_y").innerHTML = "y"  +e.gamma;
 		
 	
 }
