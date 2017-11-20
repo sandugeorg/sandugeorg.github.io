35006ae7cf8857ece04789ec437f8b0bb4991f3c
sandugeorg <sandu.georg@gmail.com>
2017-11-20 18:57:47 +0200

    up

--- js/senzori.js
+++ js/senzori.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.0"
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.20.1"
 window.addEventListener("deviceorientation", on_device_orientation );
 window.addEventListener("devicemotion", on_device_motion );
 
@@ -26,4 +26,9 @@ function on_device_motion(e)
 	document.getElementById("id_acc_g_y").innerHTML = "acc_g_y=" + e.accelerationIncludingGravity.y;
 	document.getElementById("id_acc_g_z").innerHTML = "acc_g_z=" + e.accelerationIncludingGravity.z;
 	
+	var beta = Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	var gamma =  Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI; 
+	
+	document.getElementById("id_rot_x").innerHTML = "id_rot_x" + Math.round(beta * 100);
+	document.getElementById("id_rot_y").innerHTML = "id_rot_y" + Math.round(beta * 100);
 }
