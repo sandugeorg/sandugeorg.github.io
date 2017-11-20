5ea6acf9a396a084d0571715d9d0299938d59b75
sandugeorg <sandu.georg@gmail.com>
2017-11-20 19:03:00 +0200

    up

--- js/senzori.js
+++ js/senzori.js
@@ -7,9 +7,9 @@ function on_device_orientation(e)
 
 {
 	
-document.getElementById("id_z").innerHTML = "z=" + Math.round(e.alpha * 100) / 100;
-document.getElementById("id_x").innerHTML = "x=" + Math.round(e.beta * 100) / 100;
-document.getElementById("id_y").innerHTML = "y=" + Math.round(e.gamma * 100) / 100;
+document.getElementById("id_z").innerHTML = "alpha=" + Math.round(e.alpha * 100) / 100;
+document.getElementById("id_x").innerHTML = "beta=" + Math.round(e.beta * 100) / 100;
+document.getElementById("id_y").innerHTML = "gamma=" + Math.round(e.gamma * 100) / 100;
 		
 	
 }
