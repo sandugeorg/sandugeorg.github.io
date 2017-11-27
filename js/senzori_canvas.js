91801dd0de1f8f49dfe27933c8bca887bb7a3612
sandugeorg <sandu.georg@gmail.com>
2017-11-27 19:23:22 +0200

    gg

--- js/senzori_canvas.js
+++ js/senzori_canvas.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.4"
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.5"
 window.addEventListener("deviceorientation", on_device_orientation );
 
 //------------------------------
@@ -15,13 +15,25 @@ function deseneaza_cerc(unghi1, unghi2)
 	ctx.beginPath();
 	ctx.arc(200 + unghi1 * 200 / 90, 200 + unghi2 * 200 / 90, 20, 0, 2 * Math.PI);
 	ctx.fillStyle = "#FF0000";
-	ctx.fill();
+	//ctx.fill();
 	ctx.strokeStyle = "#00FF00"
 	ctx.lineWidth = "5"
 	ctx.stroke();
 
 	
+
 }
+function on_device_motion(e)
+
+{
+	var beta = Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	var gamma = -Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI; 
+	
+	deseneaza_cer( gamma, beta);
+}
+
+
+
 
 function on_device_orientation(e)
 
