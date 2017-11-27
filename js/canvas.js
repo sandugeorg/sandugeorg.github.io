23bdd37bc54a4d48ca731da9755a84666a5e87b6
sandugeorg <sandu.georg@gmail.com>
2017-11-27 17:40:42 +0200

    canavs 2

---
+++ js/canvas.js
@@ -0,0 +1,9 @@
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.0"
+
+
+var canvas = document.getElementById("id_canvas");
+var ctx = canvas.getContext("2d");
+
+ctx.beginPath();
+ctx.lineTo(100, 100);
+ctx.stroke();
