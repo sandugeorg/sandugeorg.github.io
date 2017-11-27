8de982c98dae104936e417e4fe8beb6d415544c0
sandugeorg <sandu.georg@gmail.com>
2017-11-27 18:29:27 +0200

    com translate

--- js/canvas.js
+++ js/canvas.js
@@ -4,8 +4,9 @@ document.getElementById("id_business_version").innerHTML = "Business version = 2
 var canvas = document.getElementById("id_canvas");
 var ctx = canvas.getContext("2d");
 
+ctx.translate(100, 50);
 ctx.beginPath();
-ctx.arc(100, 100, 20, 0, 2 * Math.PI);
+ctx.arc(0, 0, 20, 0, 2 * Math.PI);
 ctx.fillStyle = "#FF0000";
 ctx.fill();
 ctx.strokeStyle = "#00FF00"
