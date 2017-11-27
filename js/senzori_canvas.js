28307c4ceb154b76403b62222554eebafb419160
sandugeorg <sandu.georg@gmail.com>
2017-11-27 18:59:27 +0200

    clear

--- js/senzori_canvas.js
+++ js/senzori_canvas.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.0"
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.1"
 window.addEventListener("deviceorientation", on_device_orientation );
 
 //------------------------------
@@ -8,6 +8,7 @@ function deseneaza_cerc(unghi1, unghi2)
 	
 	var canvas = document.getElementById("id_canvas");
 	var ctx = canvas.getContext("2d");
+	ctx.clearRect(0, 0, 400, 400);
 
 	ctx.beginPath();
 	ctx.arc(200 + unghi1 * 200 / 90, 200 + unghi2 * 200 / 90, 20, 0, 2 * Math.PI);
@@ -23,7 +24,7 @@ function deseneaza_cerc(unghi1, unghi2)
 function on_device_orientation(e)
 
 {
-	deseneaza_cerc(e.beta, e.gamma);
+	deseneaza_cerc(e.gamma, e.beta);
 	
 
 }
