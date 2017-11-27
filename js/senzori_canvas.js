c2ffbfad07dbd2723d5b5a74d4b2d6d13c185301
sandugeorg <sandu.georg@gmail.com>
2017-11-27 19:14:35 +0200

    ff

--- js/senzori_canvas.js
+++ js/senzori_canvas.js
@@ -10,7 +10,7 @@ function deseneaza_cerc(unghi1, unghi2)
 	var ctx = canvas.getContext("2d");
 	ctx.clearRect(0, 0, 400, 400);
 	ctx_rect(0, 0, 400, 400);
-	ctx.stroke();
+	//ctx.stroke();
 
 	ctx.beginPath();
 	ctx.arc(200 + unghi1 * 200 / 90, 200 + unghi2 * 200 / 90, 20, 0, 2 * Math.PI);
