ada8fe4dc8922438836c98462df23533eb4c0fd0
sandugeorg <sandu.georg@gmail.com>
2017-11-27 18:04:42 +0200

    triunghi

--- js/canvas.js
+++ js/canvas.js
@@ -1,9 +1,12 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.0"
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.27.1"
 
 
 var canvas = document.getElementById("id_canvas");
 var ctx = canvas.getContext("2d");
 
 ctx.beginPath();
+ctx.moveTo(0, 0);
 ctx.lineTo(100, 100);
+ctx.lineTo(120, 50);
+ctx.closePath();
 ctx.stroke();
