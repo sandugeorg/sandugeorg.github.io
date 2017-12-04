1cb6b14f206b42d3280d4081b92906f527ef771b
sandugeorg <sandu.georg@gmail.com>
2017-12-04 19:37:14 +0200

    s

--- js/touch.js
+++ js/touch.js
@@ -1,21 +1,19 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.1";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.04.1";
+
 
 var canvas = document.getElementById("id_canvas");
-canvas.addEventListener("touchstart", on_start_touch);
 var context = canvas.getContext("2d");
-
-//-------------------------------------------------------
-
-
+canvas.addEventListener("touchstart",on_start_touch);
+//---------------------------------------
 function on_start_touch(e)
 {
-       var touches = e.changedTouches;
-	   for (var i = 0; i < touches.length; i++)
-		 
-	       context.beginPath();
-		   context.arc(touches.item(i).pageX, touches.item(i).pageY, 20, 0, 2 * Math.PI);
-		   context.stroke();
-	   
-	]
-
-//--------------------------------------------------------
+var touches = e.changedTouches;
+for(var i=0; i<touches.length; i++)
+{
+	context.beginPath();
+	context.arc(touches.item(i).pageX,touches.item(i).pageY,20,0,2*Math.PI);
+	context.lineWidth=5;
+	context.stroke(); 
+}
+	
+}
