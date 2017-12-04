7982484a10d020a718711ca1e9fbc5b6eb41d8ea
sandugeorg <sandu.georg@gmail.com>
2017-12-04 18:26:53 +0200

    unghiuri

--- js/senzori_canvas.js
+++ js/senzori_canvas.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.1";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.2";
 
 //window.addEventListener("deviceorientation", on_device_orientation);
 window.addEventListener("devicemotion", on_device_motion);
@@ -6,13 +6,14 @@ window.addEventListener("devicemotion", on_device_motion);
 var canvas = document.getElementById("id_canvas");
 var ctx = canvas.getContext("2d");
 
-var gamma = 0;
-var beta = 0;
-setInterval(deseneaza_cerc, 40);//, gamma, beta);
+
+
+var unghiuri = {gamma:0, beta:0};
+setInterval(deseneaza_cerc, 40, unghiuri);
 
 
 //-----------------------------------------------------
-function deseneaza_cerc();//(unghi1, unghi2)
+function deseneaza_cerc(u)
 {
 		ctx.clearRect(0, 0, 400, 400);
 		
@@ -20,7 +21,7 @@ function deseneaza_cerc();//(unghi1, unghi2)
 		ctx.stroke();
 		
 		ctx.beginPath();
-		ctx.arc(200 + gamma * 200 / 90, 200 + beta * 200 / 90, 20, 0, 2 * Math.PI);
+		ctx.arc(200 + u.gamma * 200 / 90, 200 + u.beta * 200 / 90, 20, 0, 2 * Math.PI);
 		ctx.fillStyle = "#FF0000";
 		//ctx.fill();
 		ctx.strokeStyle = "#00FF00";
@@ -31,15 +32,15 @@ function deseneaza_cerc();//(unghi1, unghi2)
 function on_device_orientation(e)
 {
 	//deseneaza_cerc(e.gamma, e.beta);
-	gamma = e.gamma;
-	beta = e.beta;
+	unghiuri.gamma = e.gamma;
+	unghiuri.beta = e.beta;
 	
 }
 //-----------------------------------------------------
 function on_device_motion(e)
 {
-	 beta = Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
-	 gamma = -Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	 unghiuri.beta = Math.atan(e.accelerationIncludingGravity.y / e.accelerationIncludingGravity.z) * 180 / Math.PI;
+	 unghiuri.gamma = -Math.atan(e.accelerationIncludingGravity.x / e.accelerationIncludingGravity.z) * 180 / Math.PI;
 	
 	//deseneaza_cerc(gamma, beta);
 }
