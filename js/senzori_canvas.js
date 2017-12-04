2178bc47a2aff107d5647d402c971888325296b4
sandugeorg <sandu.georg@gmail.com>
2017-12-04 18:03:55 +0200

    1

--- js/senzori_canvas.js
+++ js/senzori_canvas.js
@@ -1,4 +1,4 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.0";
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.11.28.1";
 
 //window.addEventListener("deviceorientation", on_device_orientation);
 window.addEventListener("devicemotion", on_device_motion);
@@ -8,11 +8,11 @@ var ctx = canvas.getContext("2d");
 
 var gamma = 0;
 var beta = 0;
-setInterval(deseneaza_cerc, 40, gamma, beta);
+setInterval(deseneaza_cerc, 40);//, gamma, beta);
 
 
 //-----------------------------------------------------
-function deseneaza_cerc(unghi1, unghi2)
+function deseneaza_cerc();//(unghi1, unghi2)
 {
 		ctx.clearRect(0, 0, 400, 400);
 		
@@ -20,7 +20,7 @@ function deseneaza_cerc(unghi1, unghi2)
 		ctx.stroke();
 		
 		ctx.beginPath();
-		ctx.arc(200 + unghi1 * 200 / 90, 200 + unghi2 * 200 / 90, 20, 0, 2 * Math.PI);
+		ctx.arc(200 + gamma * 200 / 90, 200 + beta * 200 / 90, 20, 0, 2 * Math.PI);
 		ctx.fillStyle = "#FF0000";
 		//ctx.fill();
 		ctx.strokeStyle = "#00FF00";
