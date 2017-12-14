497c5803332d1ddd38de4ee18158b56a56c052ce
sandugeorg <sandu.georg@gmail.com>
2017-12-14 18:36:02 +0200

    cocos

--- js/touch.js
+++ js/touch.js
@@ -1,9 +1,11 @@
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.11.0"
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.0"
 
 var canvas = document.getElementById("id_canvas");
 var context = canvas.getContext("2d");
 
 canvas.addEventListener("touchstart", on_start_touch);
+canvas.addEventListener("touchmove", on_move_touch);
+
 
 var lista_culori = ["#FF0000", "#00FF00", "#0000FF", "#FFFF00", "#FF00FF", "#FFFFFF"];
 //---------------------------------------
@@ -18,3 +20,12 @@ function on_start_touch(e)
 	}
 }
 //---------------------------------------
+function on_move_touch(e)
+{
+	var touches = e.touches; 
+	for (var i = 0; i < touches.length; i++){
+		context.beginPath();
+		context.arc(touches.item(i).pageX, touches.item(i).pageY, 20, 0, 2 * Math.PI);
+		context.strokeStyle = lista_culori[i];
+		context.stroke();
+	}
