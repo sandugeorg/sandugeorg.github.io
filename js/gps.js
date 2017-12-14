879801634814d294272a03fa1c9f2721cac0b3cf
sandugeorg <sandu.georg@gmail.com>
2017-12-14 19:31:58 +0200

    v

--- js/gps.js
+++ js/gps.js
@@ -1,5 +1,5 @@
 
-document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.0"
+document.getElementById("id_business_version").innerHTML = "Business version = 2017.12.14.0";
 navigator.geolocation.getCurrentPosition(on_position_failure, on_position_success);
 
 function on_position_success(e)
