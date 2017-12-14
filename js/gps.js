03f93625ab22a3136edc1fc2fed8e1052a335155
sandugeorg <sandu.georg@gmail.com>
2017-12-14 19:21:26 +0200

    acc

--- js/gps.js
+++ js/gps.js
@@ -4,9 +4,9 @@ navigator.geolocation.getCurrentPosition(on_position_failure, on_position_succes
 
 function on_position_success(e)
 {
-	document.getElementById("id_lat").innerHTML = e.coords.latitude;
-	document.getElementById("id_long").innerHTML = e.coords.longitude;
-	
+	document.getElementById("id_lat").innerHTML = "Latitude =" + e.coords.latitude;
+	document.getElementById("id_long").innerHTML = "Longitude" + e.coords.longitude;
+	document.getElementById("id_acc").innerHTML = "Accuracy" +  e.coords.accuracy + m;
 }
 //-----------------------------
 function on_position_failure(e)
