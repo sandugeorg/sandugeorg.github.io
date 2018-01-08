56e8464ecf104eaf68098b6afea0f9c04508e252
sandugeorg <sandu.georg@gmail.com>
2018-01-08 17:37:38 +0200

    gps2

--- js/gps.js
+++ js/gps.js
@@ -8,7 +8,7 @@ function on_position_success(e)
 	document.getElementById("id_long").innerHTML = "Longitude" + e.coords.longitude;
 	document.getElementById("id_acc").innerHTML = "Accuracy" +  e.coords.accuracy + m;
 var map_str = "https://maps.googleapis.com/maps/api/staticmap?center="+ "e.coords.latitude+",
-"+ e.coords.longitude+","&zoom = 15" + "&size = 400x300" + "& key = AIzaSyDUU2mhTvUfY8EYOmp55I3D5OzfsBTy6vQ ";
+"+ e.coords.longitude+","&zoom = 15" + "&size = 400x300" + "&key = AIzaSyDUU2mhTvUfY8EYOmp55I3D5OzfsBTy6vQ ";
 	document.getElementById("id_img").src = map_str
 }
 //-----------------------------
