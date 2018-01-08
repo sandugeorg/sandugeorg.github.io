19b71e801d1925681e87a790ebe9dd940064bb3c
sandugeorg <sandu.georg@gmail.com>
2018-01-08 17:42:25 +0200

    bb

--- js/gps.js
+++ js/gps.js
@@ -5,8 +5,9 @@ navigator.geolocation.getCurrentPosition(on_position_failure, on_position_succes
 function on_position_success(e)
 {
 	document.getElementById("id_lat").innerHTML = "Latitude =" + e.coords.latitude;
-	document.getElementById("id_long").innerHTML = "Longitude" + e.coords.longitude;
-	document.getElementById("id_acc").innerHTML = "Accuracy" +  e.coords.accuracy + m;
+	document.getElementById("id_long").innerHTML = "Longitude =" + e.coords.longitude;
+	document.getElementById("id_acc").innerHTML = "Accuracy =" +  e.coords.accuracy + m;
+	document.getElementById("id_alt").innerHTML = "Altitude =" e.coords.altitude;
 var map_str = "https://maps.googleapis.com/maps/api/staticmap?center="+ "e.coords.latitude+",
 "+ e.coords.longitude+","&zoom = 15" + "&size = 400x300" + "&key = AIzaSyDUU2mhTvUfY8EYOmp55I3D5OzfsBTy6vQ ";
 	document.getElementById("id_img").src = map_str
