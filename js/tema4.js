cc06ecf91213fa0d148b4cb5c8a0d423ab82a975
sandugeorg <sandu.georg@gmail.com>
2018-01-27 19:00:58 +0200

    test

--- js/tema4.js
+++ js/tema4.js
@@ -1,25 +1,28 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".5"; 
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 //_______________________________________________###____________________________________________
-var image = document.getElementById("id_img");
-image.addEventListener("touchstart",on_touch_start);
-image.addEventListener("touchend",on_touch_end);
-var constraints={audio: true,video: true};
+var constraints={audio: true,video: { facingMode: { exact: "environment" } }};
 navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
 var video = document.getElementById("id_video");
-function on_touch_start(e)
-{
-	e.preventDefault();
-timer_id = setInterval(start_vibrate,100);
+video.addEventListener("touchstart",on_touch_video);
+video.addEventListener("mousedown",on_touch_video);
 
-}
-function start_vibrate()
+
+//------------------------
+function on_cam(stream)
 {
-	
-video.srcObject=stream
+	video.srcObject=stream;
 }
-function on_touch_end(e)
+//--------------------
+function on_error(e)
 {
-	e.preventDefault();
-	clearInterval(timer_id);
+	alert("ERROR!: cannot connect to camera!");
+}
+//-------------------------
+function on_touch_video(e)
+{
+	var canvas = document.getElementById("id_img");
+	var context = canvas.getContext("2d");
+	context.drawImage(video,0,0);
+
 }
