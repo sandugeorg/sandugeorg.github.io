74c6356f935b240921f3992cf78e24e01ccf80e5
sandugeorg <sandu.georg@gmail.com>
2018-01-27 16:22:55 +0200

    fsesg

--- js/camera.js
+++ js/camera.js
@@ -4,15 +4,20 @@ document.getElementById("id_business_version").innerHTML = "Business version = "
 var constraints={audio: true,video: true};
 navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
 var video = document.getElementById("id_video");
+var image = document.getElementById("id_img");
 
-//^^^^^^^^^^^^^^^^6
-function on_cam(stream)
+//----------------------------
+function on_touch_start(e)
 {
 	video.srcObject=stream;
 }
-//^^^^^^^^^^^^^^^^6
+//function on_cam(stream)
+//{
+	//video.srcObject=stream;
+//}
+//----------------------------
 function on_error(e)
 {
 	alert("ERROR!: cannot connect to camera!");
 }
-//^^^^^^^^^^^^^^^^6
+//----------------------------
