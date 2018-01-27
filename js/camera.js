a600f21044828a4f37f4b14ef86b77864404a661
sandugeorg <sandu.georg@gmail.com>
2018-01-27 16:29:18 +0200

    img  start

--- js/camera.js
+++ js/camera.js
@@ -5,6 +5,9 @@ var constraints={audio: true,video: true};
 navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
 var video = document.getElementById("id_video");
 var image = document.getElementById("id_img");
+image.addEventListener("touchstart",on_touch_start);
+image.addEventListener("touchend",on_touch_end);
+
 
 //----------------------------
 function on_touch_start(e)
