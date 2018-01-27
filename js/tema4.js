3165577e3d50ea5a2df90027c80c5fb1c383c683
sandugeorg <sandu.georg@gmail.com>
2018-01-27 20:33:51 +0200

    v5

--- js/tema4.js
+++ js/tema4.js
@@ -2,7 +2,7 @@ var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 //_______________________________________________###____________________________________________
 var constraints={audio: true,video: { facingMode: { exact: "environment" } }};
-navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
+navigator.mediaDevices.getUserMedia(constraints).then(Record).catch(on_error);
 var video = document.getElementById("id_video");
 video.addEventListener("touchstart",on_touch_video);
 video.addEventListener("mousedown",on_touch_video);
