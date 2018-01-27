16a6e6557e36f23d7c926cca15660ec90a37f845
sandugeorg <sandu.georg@gmail.com>
2018-01-27 20:43:46 +0200

    v0

--- js/tema4.js
+++ js/tema4.js
@@ -2,24 +2,23 @@ var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 //_______________________________________________###____________________________________________
 var constraints={audio: true,video: { facingMode: { exact: "environment" } }};
-navigator.mediaDevices.getUserMedia(constraints).then(Record).catch(on_error);
+navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
 var video = document.getElementById("id_video");
 video.addEventListener("touchstart",on_touch_video);
 video.addEventListener("mousedown",on_touch_video);
 
 
-//------------------------
-
-function Record(stream)
+//------------------------------
+function on_cam(stream)
 {
 	video.srcObject=stream;
 }
-//--------------------
+//-----------------------------
 function on_error(e)
 {
 	alert("ERROR!: cannot connect to camera!");
 }
-//-------------------------
+//-----------------------------
 function on_touch_video(e)
 {
 	var canvas = document.getElementById("id_img");
