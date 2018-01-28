8e4963b7f5b5a2d01186ad8be94444082dba4c35
sandugeorg <sandu.georg@gmail.com>
2018-01-28 14:10:01 +0200

    v1

--- js/tema4.js
+++ js/tema4.js
@@ -1,31 +1,20 @@
 var d = new Date();
-document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
-//_______________________________________________###____________________________________________
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".12"; 
+navigator.geolocation.getCurrentPosition(on_position_success,on_position_failure);
 
 
-var constraints={audio: true,video: { facingMode: { exact: "environment" } }};
-navigator.mediaDevices.getUserMedia(constraints).then(Start).catch(on_error);
-var video = document.getElementById("id_video");
-video.addEventListener("touchstart",on_touch_video);
-video.addEventListener("mousedown",on_touch_video);
-
-
-
-//^^^^^^^^^^^^^^^^
-function on_cam(stream)
+<script language="javascript">
+function check(form)
 {
-video.srcObject=stream;
-}
-//^^^^^^^^^^^^^^^^6
-function on_error(e)
-{
-	alert("ERROR!: cannot connect to camera!");
-}
-//^^^^^^^^^^^^^^^^6
-function on_touch_video(e)
-{
-	var canvas = document.getElementById("id_img");
-	var context = canvas.getContext("2d");
-	context.drawImage(video,0,0);
 
+if(form.userid.value == "Sandu" && form.pwd.value == "tema4")
+{
+	return true;
 }
+else
+{
+	alert("Error Password or Username")
+	return false;
+}
+}
+</script>
