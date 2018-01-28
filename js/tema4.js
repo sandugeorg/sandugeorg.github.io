9e4cf73047c6162c47ffc4f7c114d5401fced405
sandugeorg <sandu.georg@gmail.com>
2018-01-28 16:03:56 +0200

    start stop

--- js/tema4.js
+++ js/tema4.js
@@ -1,22 +1,38 @@
 var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 //_______________________________________________###____________________________________________
-var constraints={audio: true,video: { facingMode: { exact: "environment" } }};
-navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
+var constraints={
+  audio: true,
+  video: true
+};
+
 var video = document.getElementById("id_video");
-video.addEventListener("touchstart",on_touch_video);
-video.addEventListener("mousedown",on_touch_video);
 
+window.startVideo = function () {
+  navigator.mediaDevices.getUserMedia(constraints).then(on_cam).catch(on_error);
+}
 
+window.stopVideo = function () {
+  if (window.STREAM) {
+    console.log(window.STREAM);
+    var tracks = window.STREAM.getTracks();
+    tracks.forEach(function(track){
+      console.log(track);
+      track.stop();
+    });
+  }
+}
 //^^^^^^^^^^^^^^^^
 function on_cam(stream)
 {
+    window.STREAM = stream;
 	video.srcObject=stream;
 }
 //^^^^^^^^^^^^^^^^6
 function on_error(e)
 {
-	alert("ERROR!: cannot connect to camera!");
+	console.log(e);
+  alert("ERROR!: cannot connect to camera!");
 }
 //^^^^^^^^^^^^^^^^6
 function on_touch_video(e)
