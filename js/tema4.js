faad0d741a49390444231be731fc98d3b9a66e9f
sandugeorg <sandu.georg@gmail.com>
2018-01-28 15:02:10 +0200

    bb

--- js/tema4.js
+++ js/tema4.js
@@ -1,25 +1,7 @@
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
-var textarea = document.getElementById("code");
-var button = document.getElementById("button");
-var reset = document.getElementById("reset");
-var edit = document.getElementById("edit");
-var code = textarea.value;
-button.focus();
+<a href="tel:0747848747">
+    Call number!
+</a>
 
-function drawCanvas() {
-  ctx.clearRect(0, 0, canvas.width, canvas.height);
-  eval(textarea.value);
-}
-
-reset.addEventListener("click", function() {
-  textarea.value = code;
-  drawCanvas();
-});
-
-edit.addEventListener("click", function() {
-  textarea.focus();
-})
-
-textarea.addEventListener("input", drawCanvas);
-window.addEventListener("load", drawCanvas);
+<a href="sms:0747848747?body=Hello%20there!">
+    Compose SMS!
+</a>
