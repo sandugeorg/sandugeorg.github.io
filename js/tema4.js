50a12e2bcf09e726a1100d3f7142eb895443521d
sandugeorg <sandu.georg@gmail.com>
2018-01-28 16:34:12 +0200

    bat

--- js/tema4.js
+++ js/tema4.js
@@ -1,6 +1,17 @@
 var d = new Date();
 document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 //_______________________________________________###____________________________________________
+navigator.getBattery().then(on_battery).catch(on_battery_error);
+function on_battery(b)
+{
+document.getElementById("id_battery").innerHTML = b.level + " " + b.charging;	
+	1
+}
+function on_battery_error()
+{
+	
+	alert("cannot read battery!");
+}
 var constraints={
   audio: true,
   video: true
