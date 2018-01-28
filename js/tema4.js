3a57ec06d2c4ed749ddc7eac2f1f47821248a72f
sandugeorg <sandu.georg@gmail.com>
2018-01-28 23:16:38 +0200

    pp

--- js/tema4.js
+++ js/tema4.js
@@ -1,5 +1,5 @@
-//var d = new Date();
-//document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
+var d = new Date();
+document.getElementById("id_business_version").innerHTML = "Business version = " + d.getFullYear() + "." + (d.getMonth()+1) + "." + d.getDate() + ".3"; 
 //_______________________###____________________________________________
 var constraints={
   audio: true,
